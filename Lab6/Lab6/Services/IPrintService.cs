ad023a1da9811bb86e5fe3fe64d98c3c04529401
RuslanTsimbalyuck <ipz234_tsri@student.ztu.edu.ua>
2025-05-19 20:51:07 +0300

    Extract IPrintService interface and improve PrintService SRP

---
+++ Lab6/Lab6/Services/IPrintService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6.Services
+{
+	public interface IPrintService
+	{
+		void PrintQRCode(string filePath);
+	}
+}
+
--- Lab6/Lab6/Services/PrintService.cs
+++ Lab6/Lab6/Services/PrintService.cs
@@ -1,28 +1,37 @@
 ﻿using System;
 using System.Drawing.Printing;
+using System.Drawing;
 
 namespace Lab6.Services
 {
-    public class PrintService
+    public class PrintService : IPrintService
     {
         public void PrintQRCode(string filePath)
         {
             PrintDocument printDoc = new PrintDocument();
-            printDoc.PrintPage += (sender, e) =>
-            {
-                try
-                {
-                    using (var image = System.Drawing.Image.FromFile(filePath))
-                    {
-                        e.Graphics.DrawImage(image, 10, 10, 300, 300);
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"Error printing: {ex.Message}");
-                }
-            };
+            printDoc.PrintPage += (sender, e) => HandlePrintPage(e, filePath);
             printDoc.Print();
+        }
+
+        private void HandlePrintPage(PrintPageEventArgs e, string filePath)
+        {
+            try
+            {
+                using (Image image = Image.FromFile(filePath))
+                {
+                    e.Graphics.DrawImage(image, 10, 10, 300, 300);
+                }
+            }
+            catch (Exception ex)
+            {
+                LogError(ex);
+            }
+        }
+
+        private void LogError(Exception ex)
+        {
+            // Можна замінити на більш розширену систему логування
+            Console.WriteLine($"[PrintService] Error printing QR Code: {ex.Message}");
         }
     }
 }
